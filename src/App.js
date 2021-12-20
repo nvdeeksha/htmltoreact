5e2c5a8d96568028ece7e580adfba5e0467abc78
nvdeeksha <deeksha.nv@grssl.net>
2021-12-20 18:08:58 +0530

    multipage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Meeting2 from './pages/Meeting2';
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+} from 'react-router-dom';
+
+export default function App() {
+  return(
+    <React.Fragment>
+      <Router>
+      <div>
+        <Switch>
+          <Route path="/"exact>
+            <Home />
+
+          </Route>
+          <Route path="/Meeting2">
+            <Meeting2 />
+
+          </Route>
+
+        </Switch>
+        </div>
+        </Router>
+        </React.Fragment>
+  )
 }
-
-export default App;
+   
